a11de44e0a3ff3de0b286d4c823bbe57046d5080
Victor <victoraugusto146533@gmail.com>
2025-09-25 18:45:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -139,4 +139,5 @@ def devolver():
         return jsonify({'message': 'Erro ao devolver o livro. Livro não encontrado ou não estava emprestado.'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
